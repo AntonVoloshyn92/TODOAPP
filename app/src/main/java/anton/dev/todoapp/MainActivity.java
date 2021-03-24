d9056fd5cfb851d55b6dd1251de4f92b06b89adf
AntonVoloshyn92 <sverin22@gmail.com>
2021-03-24 10:05:00 +0200

    first commit x

---
+++ app/src/main/java/anton/dev/todoapp/MainActivity.java
@@ -0,0 +1,49 @@
+package anton.dev.todoapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements MainContract.View{
+
+    private RecyclerViewAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Task task;
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("WTF", "Click button");
+                //todo build MVP
+                //build controller and local DB and Remute DB
+                //todo set firebase
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public void showTodoList(List<String> todoList) {
+
+    }
+}
---
+++ app/src/main/java/anton/dev/todoapp/Task.java
@@ -0,0 +1,16 @@
+package anton.dev.todoapp;
+
+public class Task {
+    private String description;
+
+    public Task() {
+    }
+
+    public Task(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
